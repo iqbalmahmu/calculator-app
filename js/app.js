d8b468165fadd0417607b5c8c51e0f2192f1b3d8
foysal ahmed <mdrad43@gmail.com>
2021-07-10 00:24:19 +0800

    added javascript file

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const numberButton = document.querySelectorAll("[data-number]");
+
+const oparandButton = document.querySelectorAll("[data-oparand]");
+
+const equalButton = document.querySelector("[data-equal]");
+
+const allClearButton = document.querySelector("[data-all-clear]");
+
+const delateButton = document.querySelector("[data-delate]");
+
+const clearButton = document.querySelector("[data-clear]");
+
+const previusButton = document.querySelector("[data-previus-oparand]");
+
+const currentButton = document.querySelector("[data-current-oparand]");
---
+++ js/dist/app.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var numberButton = document.querySelectorAll("[data-number]");
+var oparandButton = document.querySelectorAll("[data-oparand]");
+var equalButton = document.querySelector("[data-equal]");
+var allClearButton = document.querySelector("[data-all-clear]");
+var delateButton = document.querySelector("[data-delate]");
+var clearButton = document.querySelector("[data-clear]");
+var previusButton = document.querySelector("[data-previus-oparand]");
+var currentButton = document.querySelector("[data-current-oparand]");
